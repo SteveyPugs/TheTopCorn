51eb54e78692d3bb9df3342060dbd67772faf9a6
Stephen Pugliese <stephen.pugliese@outlook.com>
2019-06-18 21:26:45 -0400

    Remove all existing code

--- static/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("Pop", ["ui.bootstrap", "ngMap", "ngFileUpload"]).filter("range", function(){
-	return function(input, total){
-		total = parseInt(total);
-		for (var i=0; i<total; i++){
-			input.push(i);
-		}
-		return input;
-	};
-});
--- static/login.controller.js
+++
@@ -1,1 +0,0 @@
-app.controller("Login", function($scope){});
